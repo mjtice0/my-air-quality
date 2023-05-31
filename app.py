83cb80168493419efe618c990364d14f4fed0b2b
maggie <mjtice0@gmail.com>
2023-05-30 19:05:51 -0600

    add html registration template and register route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import requests
 import os 
 from dotenv import load_dotenv
@@ -7,6 +7,7 @@ import time
 from send_sms import * 
 
 load_dotenv()
+
 app = Flask(__name__)
 
 # Api key variables
@@ -16,6 +17,18 @@ my_api_write_key = os.getenv("API_WRITE_KEY")
 @app.route("/")
 def home():
     return '<h1>Hello, Air Quality</h1>'
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        phone = request.form['phone']
+        aqi_threshold = request.form['aqi_threshold']
+
+        return "Registration successful!"
+
+    return render_template("registration.html")
 
 @app.route("/search")
 def search():
@@ -84,96 +97,8 @@ def API_call_schedule():
 if __name__ == '__main__':
     app.run()
 
-    
-
-# # Function to make API call to get sensor data 
-# def call_API():
-#     my_url = "https://api.purpleair.com/v1/sensors/177595"
-#     my_headers = {'X-API-Key':API_READ_KEY}
-#     response = requests.get(my_url, headers=my_headers)
-#     purple_air_data = response.json()
-#     result = purple_air_data['sensor']['pm2.5']
-#     print("API call executed")
-#     # print(result)
-#     return result
-# # Function to convert pm25 raw number to AQI, calls calculate AQI function
-# def convert_to_AQI(result):
-#     pm25 = result
-
-#     if pm25 < 0:
-#         return pm25
-    
-#     if pm25 > 250.5:
-#         return calculate_AQI(pm25, 500, 301, 500.4, 250.5)
-#     elif pm25 > 150.5:
-#         return calculate_AQI(pm25, 300, 201, 250.4, 150.5)
-#     elif pm25 > 55.5:
-#         return calculate_AQI(pm25, 200, 151, 150.4, 55.5)
-#     elif pm25 > 35.5:
-#         return calculate_AQI(pm25, 150, 101, 55.4, 35.5)
-#     elif pm25 > 12.1:
-#         return calculate_AQI(pm25, 100, 51, 35.4, 12.1)
-#     elif pm25 >= 0:
-#         return calculate_AQI(pm25, 50, 0, 12, 0)
-#     else:
-#         return None
-
-# # Function to calculate pm2.5 to US aqi
-# def calculate_AQI(Cp, IHi, ILo, BPHi, BPLo):
-#     a = (IHi - ILo)
-#     b = (BPHi - BPLo)
-#     c = (Cp - BPLo)
-#     aqi = round(((a / b) * c + ILo))
-#     print("Calculation executed")
-#     return aqi
-
-# # Function to initiate sms text alert
-# # Calls send_sms function
-# def send_alert(aqi):
-#     if aqi > 100:
-#         send_sms(aqi)
-
-# air_quality_list = []
-# # Function to add data to database -- will be updated to work with db
-# def save_to_database(aqi):
-#     air_quality_list.append(aqi)
-
-#     print(air_quality_list)
-
-# # Function to send get request at intervals -- Data is updated every two minutes, retrieve data every hour
-# # Currently on 10s schedule for testing 
-# # Change order of logic, call API_call_schedule in call_API function instead
-# def API_call_schedule():
-#     def scheduled_call():
-#         API_response = call_API()
-
-#         aqi = convert_to_AQI(API_response)
-        
-#         send_alert(aqi)
-
-#         save_to_database(aqi)
-
-#     schedule.every(10).seconds.do(scheduled_call)
-
-#     while True:
-#         schedule.run_pending()
-#         time.sleep(1)
-
-# API_call_schedule()
-
 
 # # Add logic to check if aqi value is the same as the previous value, if it is, don't send alert
 # # Can update sms send logic to send message based on aqi level -- if aqi < 50, sms = air quality is good
 # # Will need to add error logic for sequence of function calls so program does not break
 
-# # Things to consider:
-# # - how long will intervals go
-# # - memoize data
-# # - how event is triggered by user
-
-# # Formula to convert PM to AQI from Airnow.gov:
-# # Cp = the truncated concentration of pollutant p
-# # BPHi = the concentration breakpoint that is greater than or equal to Cp 
-# # BPLo = the concentration breakpoint that is less than or equal to Cp
-# # IHi = the AQI value corresponding to BPHi
-# # ILo = the AQI value corresponding to BPLo
